a24de5b9d1b862aaf95f5f17614b75d043598fe4
GUSE\TesSf <tesfov@gmail.com>
2024-10-29 11:17:51 +0100

    no message

---
+++ Laboratorium 2/Zadanie4.py
@@ -0,0 +1,24 @@
+from functools import reduce
+import numpy as np
+
+
+def Zadanie4(macierz, operacja):
+
+    try:
+        result = reduce(lambda x,y: eval(operacja), macierz)
+        return result
+    
+    except ValueError:
+        print('Wystąpił błąd')
+
+
+
+operacja = "x * y"
+
+macierz = [
+    np.array([[1,2],[3,4]]),
+    np.array([[5,6],[7,8]]),
+    np.array([[9,10],[11,12]])
+]
+
+print(Zadanie4(macierz,operacja))
